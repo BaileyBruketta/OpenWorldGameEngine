a7393323873f91e63666adc2b52c04d24dbc9b81
bailey bruketta <bruketta.bailey@gmail.com>
2019-10-04 05:37:24 -0500

    added inventory system base

---
+++ EquipmentSlots.cs
@@ -0,0 +1,11 @@
+﻿
+public class EquipmentSlots : Itemslots
+{
+    public EquipmentType EquipmentType;
+
+    protected override private void OnValidate()
+    {
+        base.OnValidate();
+        gameObject.name = EquipmentType.ToString() + " Slot";
+    }
+}
---
+++ EquippableItem.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+public enum EquipmentType
+{
+    Gun,
+    Knife
+}
+
+[CreateAssetMenu]
+
+
+public class EquippableItem : Item
+{
+    public int Strengthbonus;
+    public int Agilitybonus;
+    public int Vitalitybonus;
+    public int Dexteritybonus;
+    public int Powerbonus;
+
+    [Space]
+
+    public EquipmentType EquipmentType;
+
+
+
+}
